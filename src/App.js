b0781029338bc66460d7835ebcc15903b7a02a53
Zalbo <zjagema@gmail.com>
2023-11-22 23:45:28 +0300

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Movies from './components/movie';
-import Nav from './components/nav';
-import TvSeries from './components/tv';
-import Home from './components/home';
-import Footer from './components/footer';
-import Search from './components/search';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movie";
+import Nav from "./components/nav";
+import TvSeries from "./components/tv";
+import Home from "./components/home";
+import Footer from "./components/footer";
+import Search from "./components/search";
 
 function App() {
   return (
-    <Router>
-
+    <Router basename="/React_Movie_App">
       <div className="App">
-
-        
-      
-         <Nav />
-       
-         
-
+        <Nav />
         <Routes>
-
-          <Route exact path='/' element={<Home />}/>
-          
-          <Route  path='/tv' element={<TvSeries />}/>
-           
-         
-          <Route exact path='/movie' element={<Movies />}/>
-         
-         <Route path='/srch' element={<Search />} />
-       
-             
+          <Route exact path="/" element={<Home />} />
+          <Route path="/tv" element={<TvSeries />} />
+          <Route path="/movie" element={<Movies />} />
+          <Route path="/srch" element={<Search />} />
         </Routes>
-      
-        <Footer /> 
-        
+        <Footer />
       </div>
-      
-    
-     </Router>
+    </Router>
   );
 }
 
